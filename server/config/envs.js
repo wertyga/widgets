6ccd4d5c1813e6302e1774cf5f7d111714364b00
wertyga <wertyga18@gmail.com>
2020-12-11 21:02:17 +0300

    ch envs

--- server/config/envs.js
+++ server/config/envs.js
@@ -6,6 +6,6 @@ module.exports = {
   SERVER_HOST: process.env.NODE_ENV !== 'production' ? `http://localhost:${PORT}` : `http://134.209.126.140:${PORT}`,
   IO_SERVER_PORT,
   REDIS_PORT: 6379,
-  IO_SERVER_NAME: process.env !== 'production' ? `http://localhost:${IO_SERVER_PORT}` : `http://134.209.126.140:${IO_SERVER_PORT}`,
-  API_GATEWAY: process.env !== 'production' ? `http://localhost:${PORT}/api` : `http://134.209.126.140:${PORT}/api`,
+  IO_SERVER_NAME: process.env.NODE_ENV !== 'production' ? `http://localhost:${IO_SERVER_PORT}` : `http://134.209.126.140:${IO_SERVER_PORT}`,
+  API_GATEWAY: process.env.NODE_ENV !== 'production' ? `http://localhost:${PORT}/api` : `http://134.209.126.140:${PORT}/api`,
 };
