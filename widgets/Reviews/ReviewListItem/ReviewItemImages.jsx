d9c195922a394cb92dcb990e0c3a6ed43f0df463
wertyga <wertyga18@gmail.com>
2020-12-27 21:06:23 +0300

    fix image styles

--- widgets/Reviews/ReviewListItem/ReviewItemImages.jsx
+++ widgets/Reviews/ReviewListItem/ReviewItemImages.jsx
@@ -5,7 +5,7 @@ export const ReviewItemImages = ({ images }) => {
     <div className="d-flex w-100">
       {images.map(path => (
         <Image
-          className="w-rv-lti__img pa-2"
+          className="w-rv-lti__img pr-2 pt-2 pb-2"
           key={path}
           src={path}
           zoom
